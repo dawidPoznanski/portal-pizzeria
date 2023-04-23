86d7b93ea8456f5cb2dc9ac1728cc6c04a376f80
Dawid Poznański <dawid.poznanski@outlook.com>
2023-04-23 22:04:54 +0200

    added sending orders to server

--- src/js/script.js
+++ src/js/script.js
@@ -392,6 +392,9 @@
         subtotalPrice: document.querySelector(select.cart.subtotalPrice),
         totalPrice: document.querySelectorAll(select.cart.totalPrice),
         totalNumber: document.querySelector(select.cart.totalNumber),
+        form: document.querySelector(select.cart.form),
+        addres: document.querySelector(select.cart.address),
+        phone: document.querySelector(select.cart.phone),
       };
 
       thisCart.dom.productList = document.querySelector(
@@ -415,6 +418,38 @@
       thisCart.dom.productList.addEventListener('remove', function (e) {
         thisCart.remove(e.detail.cartProduct);
       });
+
+      thisCart.dom.form.addEventListener('submit', function (e) {
+        e.preventDefault();
+        thisCart.sendOrder();
+      });
+    }
+
+    sendOrder() {
+      const thisCart = this;
+      const URL = settings.db.url + '/' + settings.db.orders;
+
+      const payload = {};
+
+      payload.addres = thisCart.dom.addres;
+      payload.phone = thisCart.dom.phone;
+      payload.totalPrice = thisCart.dom.totalPrice;
+      payload.subtotalPrice = thisCart.dom.subtotalPrice;
+      payload.totalNumber = thisCart.dom.totalNumber;
+      payload.deliveryFee = thisCart.dom.deliveryFee;
+      payload.products = [];
+
+      for (let prod of thisCart.products) {
+        payload.products.push(prod.getData());
+      }
+      const options = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload),
+      };
+      fetch(URL, options);
     }
 
     add(menuProduct) {
@@ -434,27 +469,27 @@
     update() {
       const thisCart = this;
 
-      let deliveryFee = settings.cart.defaultDeliveryFee;
-      let totalNumber = 0;
-      let subtotalPrice = 0;
+      thisCart.deliveryFee = settings.cart.defaultDeliveryFee;
+      thisCart.totalNumber = 0;
+      thisCart.subtotalPrice = 0;
 
       for (let product of thisCart.products) {
         if (product) {
-          totalNumber = totalNumber + product.amount;
-          subtotalPrice = subtotalPrice + product.price;
+          thisCart.totalNumber = thisCart.totalNumber + product.amount;
+          thisCart.subtotalPrice = thisCart.subtotalPrice + product.price;
         }
       }
 
-      thisCart.totalPrice = subtotalPrice;
-      if (totalNumber != 0) {
-        thisCart.totalNumber += deliveryFee;
+      thisCart.totalPrice = thisCart.subtotalPrice;
+      if (thisCart.totalNumber != 0) {
+        thisCart.totalNumber += thisCart.deliveryFee;
       } else {
-        deliveryFee = 0;
+        thisCart.deliveryFee = 0;
       }
 
-      thisCart.dom.deliveryFee.innerHTML = deliveryFee;
-      thisCart.dom.subtotalPrice.innerHTML = subtotalPrice;
-      thisCart.dom.totalNumber.innerHTML = totalNumber;
+      thisCart.dom.deliveryFee.innerHTML = thisCart.deliveryFee;
+      thisCart.dom.subtotalPrice.innerHTML = thisCart.subtotalPrice;
+      thisCart.dom.totalNumber.innerHTML = thisCart.totalNumber;
 
       for (let price of thisCart.dom.totalPrice) {
         price.innerHTML = thisCart.totalPrice;
@@ -542,6 +577,20 @@
         e.preventDefault;
         thisCartProduct.remove();
       });
+    }
+
+    getData() {
+      const thisCartProduct = this;
+      const orderData = {};
+
+      orderData.id = thisCartProduct.id;
+      orderData.amount = thisCartProduct.amount;
+      orderData.price = thisCartProduct.price;
+      orderData.priceSingle = thisCartProduct.priceSingle;
+      orderData.name = thisCartProduct.name;
+      orderData.params = thisCartProduct.params;
+
+      return orderData;
     }
   }
 
