bd539e66609b6a0b2a0dac95cd45fcf431a5fa83
Dawid Poznański <dawid.poznanski@outlook.com>
2023-05-10 21:50:13 +0200

    add Bookings methods and functions

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 import { settings, select, classNames, templates } from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
+import Booking from './components/Booking.js';
 
 const app = {
   initPages: function () {
@@ -60,6 +61,13 @@ const app = {
     }
   },
 
+  initBooking: function () {
+    const thisApp = this;
+
+    const widgetContainer = document.querySelector(select.containerOf.booking);
+    thisApp.booking = new Booking(widgetContainer);
+  },
+
   initCart: function () {
     const thisApp = this;
 
@@ -101,6 +109,7 @@ const app = {
     thisApp.initPages();
     thisApp.initData();
     thisApp.initCart();
+    thisApp.initBooking();
   },
 };
 
---
+++ src/js/components/Booking.js
@@ -0,0 +1,42 @@
+import { select, templates } from '../settings.js';
+import AmountWidget from './AmountWidget.js';
+
+class Booking {
+  constructor(element) {
+    const thisBooking = this;
+    thisBooking.render(element);
+    thisBooking.initWidgets();
+  }
+
+  render(element) {
+    const thisBooking = this;
+
+    const generateHTML = templates.bookingWidget();
+
+    thisBooking.dom = {};
+    thisBooking.dom.wrapper = element;
+    thisBooking.dom.wrapper.innerHTML = generateHTML;
+    thisBooking.dom.peopleAmount = element.querySelector(
+      select.booking.peopleAmount
+    );
+    thisBooking.dom.hoursAmount = element.querySelector(
+      select.booking.hoursAmount
+    );
+  }
+
+  initWidgets() {
+    const thisBooking = this;
+    thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
+    thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
+
+    thisBooking.dom.peopleAmount.addEventListener('click', function (e) {
+      e.preventDefault();
+    });
+
+    thisBooking.dom.hoursAmount.addEventListener('click', function (e) {
+      e.preventDefault();
+    });
+  }
+}
+
+export default Booking;
